25bd635a63d8b98682cedb459b7f4fd0f27c23d5
giumanuz <88536667+giumanuz@users.noreply.github.com>
2023-02-14 13:06:00 +0100

    add space3

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,6 +8,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
 
+
   @override
   State<Loading> createState() => _LoadingState();
 
